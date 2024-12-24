0c69885bda100666cbc73bdabb9f944916cd6759
yunsuk <yunsuk@marsauto.com>
2024-12-24 16:30:42 +0900

    Initial commit

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,16 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  opts = {
+    ensure_installed = {
+      "c",
+      "cpp",
+      "lua",
+      "python", -- 필요한 언어를 설치
+    },
+    highlight = {
+      enable = true, -- 구문 강조 활성화
+      additional_vim_regex_highlighting = false,
+    },
+  },
+}
